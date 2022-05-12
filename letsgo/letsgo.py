408f2b54fca0b7079c83484b9731444909f5fa00
Quintin Mintiens <quintin.mintiens@student.hogent.be>
2022-05-12 18:18:02 +0200

    fully functional, one bug, may be fixed when running on linux

--- letsgo/letsgo.py
+++ letsgo/letsgo.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as soup
 import PyPDF2
 import mmap
 import mysql.connector
+import os
 pdf_path = ""
 
 mydb = mysql.connector.connect(
@@ -16,8 +17,10 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor(buffered=True)
 
 companyCursor = mydb.cursor(buffered=True)
-companyCursor.execute("SELECT OndernemingsNr FROM KMO NO LIMIT")
+companyCursor.execute("SELECT OndernemingsNr FROM KMO")
 companyNumbers = companyCursor.fetchall()
+rowcount = companyCursor.rowcount
+
 
 
 
@@ -36,108 +39,110 @@ def download_file(download_url, filename):
     file.close()
 
 
-while True:
-    number = input('Enter a companynumber: ')
-    if len(number) != 10:
-        print('a company number has 10 digits')
-        continue
-    else:
-        break
-url = f'https://www.staatsbladmonitor.be/bedrijfsfiche.html?ondernemingsnummer={number}'
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-page_soup = soup(webpage, "html.parser")
-thesoup = page_soup.findAll('td', class_='data')
-l = []
 
-for a in thesoup:
-    l.append(a)
+for n in companyNumbers:
+    for i in n:
+        number = "0"+str(i)
 
-for e in l:
-    if e.text.__contains__('2020'):
-        year2020 = e
-try:
-    linkElement = year2020.findNext('a')
-    link = linkElement['href']
-except NameError:
-    print("This company number doesn't exist")
-    exit()
+    print(number)
+    url = f'https://www.staatsbladmonitor.be/bedrijfsfiche.html?ondernemingsnummer={number}'
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    page_soup = soup(webpage, "html.parser")
+    thesoup = page_soup.findAll('td', class_='data')
+    l = []
 
-print('Jaarrekening: ', link)
-download_file(link, './pdf/Jaarrekening')
-print('File downloaded')
+    for a in thesoup:
+        l.append(a)
+
+    for e in l:
+        if e.text.__contains__('2020'):
+            year2020 = e
+    try:
+        linkElement = year2020.findNext('a')
+        link = linkElement['href']
+    except NameError:
+        print("This company number doesn't exist")
+        exit()
+
+    print('Jaarrekening: ', link)
+    download_file(link, './pdf/jaarrekening')
+    print('File downloaded')
 # creating a pdf file object
-pdfFileObj = open('./pdf/Jaarrekening.pdf', 'rb')
+    pdfFileObj = open('./pdf/jaarrekening.pdf', 'rb')
 # creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 # printing number of pages in pdf file
-print('Pages: ' + str(pdfReader.numPages))
-txtfile = open('./txt/Jaarrekening' + ".txt", 'w+', encoding="utf-8")
+    print('Pages: ' + str(pdfReader.numPages))
+    txtfile = open("Jaarrekening%s.txt"%number, 'w+', encoding="utf-8")
 
-for x in range(pdfReader.numPages):
-    p = pdfReader.getPage(x)
-    t = p.extractText()
-    txtfile.write(t)
-txtfile.close()
+    for x in range(pdfReader.numPages):
+        p = pdfReader.getPage(x)
+        t = p.extractText()
+        txtfile.write(t)
+    txtfile.close()
 # closing the pdf file object
-pdfFileObj.close()
-keywords = readFile('./txt/zoektermen.txt')
+    pdfFileObj.close()
 
-with open('./txt/Jaarrekening.txt') as f:
-    s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    hits = 0
-    wordsFound = []
-    for x in keywords:
-        if s.find(bytes(x, 'utf-8')) != -1:
-            hits += 1
-            wordsFound.append(x)
-print('Number of keywords found:' + str(hits))
-print('Hits:')
+    keywords = readFile('./txt/zoektermen.txt')
+
+    with open('Jaarrekening%s.txt'%number)  as f:
+        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        hits = 0
+        wordsFound = []
+        for x in keywords:
+            if s.find(bytes(x, 'utf-8')) != -1:
+                hits += 1
+                wordsFound.append(x)
+    print('Number of keywords found:' + str(hits))
+    print('Hits:')
 # Open the file in read mode
-text = open("./txt/Jaarrekening.txt", "r", encoding="utf-8")
+    text = open("Jaarrekening%s.txt" %number, "r", encoding="utf-8")
 # Create an empty dictionary
-d = dict()
-for w in wordsFound:
-    d[w] = 0
+    d = dict()
+    for w in wordsFound:
+        d[w] = 0
 # Loop through each line of the file
-for line in text:
+    for line in text:
     # Remove the leading spaces and newline character
-    line = line.strip()
+        line = line.strip()
     # Convert the characters in line to
     # lowercase to avoid case mismatch
-    line = line.lower()
+        line = line.lower()
     # Split the line into words
-    words = line.split(" ")
+        words = line.split(" ")
     # Iterate over each word in line
-    for word in words:
+        for word in words:
         # Check if the word is already in dictionary
-        if word in d:
+            if word in d:
             # Increment count of word by 1
-            d[word] = d[word] + 1
+                d[word] = d[word] + 1
 # Print the contents of dictionary
-cmd = "INSERT INTO ZoekResultaat (zoektermid, ondernemingsnummer,aantalKeerSite, aantalKeerVerslag) VALUES (%s, %s, null, %s)"
-for key in list(d.keys()):
-    zoekterm = key
-    mycursor.execute("""SELECT id from ZoekTerm WHERE term = '%s'""" % (zoekterm))
-    id = mycursor.fetchone()
-    kv = d[key]
-    for row in id:
-        idn = row
-    if kv == 0:
-        kv = 1
-    val = (idn, number, kv)
 
-    for row in id:
-        print('id:', row)
-    if d[key] != 0:
-        print('term:', key, "\naantal:", d[key])
-    else:
-        d[key] = 1
-        print('term:', key, "\naantal:", d[key])
+    cmd = "INSERT INTO ZoekResultaat (zoektermid, ondernemingsnummer,aantalKeerSite, aantalKeerVerslag) VALUES (%s, %s, null, %s)"
+    for key in list(d.keys()):
+        zoekterm = key
+        mycursor.execute("""SELECT id from ZoekTerm WHERE term = '%s'""" % (zoekterm))
+        id = mycursor.fetchone()
+        kv = d[key]
+        for row in id:
+            idn = row
+        if kv == 0:
+            kv = 1
+        val = (idn, number, kv)
+
+        for row in id:
+            print('id:', row)
+        if d[key] != 0:
+            print('term:', key, "\naantal:", d[key])
+        else:
+            d[key] = 1
+            print('term:', key, "\naantal:", d[key])
 
 
-    mycursor.execute(cmd, val)
+        mycursor.execute(cmd, val)
 
-mydb.commit()
-
+    mydb.commit()
+    txtfile.close()
+    os.remove("Jaarrekening%s.txt" %number)
 mycursor.close()
